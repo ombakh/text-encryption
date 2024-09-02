a75fb1890c333b5c1f2b0aea1ce30e3a627bfae6
ombakhshi <ombakh28@gmail.com>
2024-09-02 17:12:59 -0400

    commenting out ethans mess

--- Vigenere.java
+++ Vigenere.java
@@ -1,27 +1,27 @@
-import java.util.Scanner;
-public class Vigenere {
-  public static void main(String[] args){
-    int[][] encryptArray = new int[2][35];
-
-  public void arrayFiller() {
-    Scanner scanner = new Scanner(System.in);
-    UserInput userInput = new UserInput();
-    for(int i = 0; i < 35; i++){
-      encryptArray[0][i] = userInput[i];
-    }
-    for (int i = 0; i < 35; ++i){
-      encryptArray[1][i] = key[i];
-    }
-
-  }
-
-  public static void printArray(int[][] encryptArray){
-    for(int i = 0; i<encryptArray.length; i++){
-      for(int j = 0; j < encryptArray[i].length; j++){
-        System.out.print(encryptArray[i][j] + " ");
-      }
-      System.out.println();
-    }
-  }
- }
-}
+//import java.util.Scanner;
+//public class Vigenere {
+//  public static void main(String[] args) {
+//    int[][] encryptArray = new int[2][35];
+//  }
+//  public void arrayFiller() {
+//    Scanner scanner = new Scanner(System.in);
+//    UserInput userInput = new UserInput();
+//    for(int i = 0; i < 35; i++){
+//      encryptArray[0][i] = userInput[i];
+//    }
+//    for (int i = 0; i < 35; ++i){
+//      encryptArray[1][i] = key[i];
+//    }
+//
+//  }
+//
+//  public static void printArray(int[][] encryptArray){
+//    for(int i = 0; i<encryptArray.length; i++){
+//      for(int j = 0; j < encryptArray[i].length; j++){
+//        System.out.print(encryptArray[i][j] + " ");
+//      }
+//      System.out.println();
+//    }
+//  }
+// }
+//}
