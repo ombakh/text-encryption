7cc0b023eda73728de7f80505e371b9ff547ae54
EthanRH11 <157135917+EthanRH11@users.noreply.github.com>
2024-09-02 20:37:41 -0400

    corrected print and array

--- Vigenere.java
+++ Vigenere.java
@@ -1,27 +1,45 @@
-//import java.util.Scanner;
-//public class Vigenere {
-//  public static void main(String[] args) {
-//    int[][] encryptArray = new int[2][35];
-//  }
-//  public void arrayFiller() {
-//    Scanner scanner = new Scanner(System.in);
-//    UserInput userInput = new UserInput();
-//    for(int i = 0; i < 35; i++){
-//      encryptArray[0][i] = userInput[i];
-//    }
-//    for (int i = 0; i < 35; ++i){
-//      encryptArray[1][i] = key[i];
-//    }
-//
-//  }
-//
-//  public static void printArray(int[][] encryptArray){
-//    for(int i = 0; i<encryptArray.length; i++){
-//      for(int j = 0; j < encryptArray[i].length; j++){
-//        System.out.print(encryptArray[i][j] + " ");
-//      }
-//      System.out.println();
-//    }
-//  }
-// }
-//}
+import java.util.Scanner;
+
+public class Vigenere {
+    public class Vigenere {
+        char[][] encryptArray = new char[2][35];
+
+        public static void main(String[] args) {
+            Vigenere vigenere = new Vigenere();
+            vigenere.arrayFiller(); //fill the array with user input and key
+            vigenere.printArray(vigenere.encryptArray); //print the filled array (debugging purposes)
+    }
+
+        public void arrayFiller() {
+            Scanner scanner + new Scanner(System.in);
+            UserInput userInput = new UserInput();
+
+            String text = userInput.getText();  //get user input text
+            StringBuilder key = userInput.keyGenerator(text);  //generate key based on user input text
+
+            for(int i = 0; i < 35; i++){
+                if(i  < text.length()){
+                    encryptArray[0][i] = text.charAt(i);    //fill first row with user input text
+                } else {
+                    encryptArray[0][i] ='';  //fill remaining spaces with space character
+                }
+            }
+
+            for(int i = 0; i  < 35; i++){
+                if(i < key.length()){
+                    encryptArray[1][i] = key.charAt(i);    //fill second row with key
+                } else {
+                    encryptArray[1][i] = ' '; //fill remaining spaces with space character
+                }
+            }
+        }
+
+        public void printArray(char[][] encryptArray){
+            for(int i = 0; i<encryptArray.length; i++){
+                for(int j = 0; j < encryptArray[i].length; j++){
+                    System.out.print(encryptArray[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
