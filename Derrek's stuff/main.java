6e3727018240b5f0df30de52c00681ff85667f2c
Anthony Cruz <anthonycruz0517@gmail.com>
2024-09-03 18:32:53 -0400

    I hate Derrek

--- Derrek's stuff/main.java
+++ Derrek's stuff/main.java
@@ -4,6 +4,7 @@
 class HelloWorld {
     // Your program begins with a call to main().
     // Prints "Hello, World" to the terminal window.
+    // I fucking hate Derrek
     public static void main(String args[])
     {
         System.out.println("Hello, World");
