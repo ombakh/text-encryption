7caee785c1ec63a8d633215114e85685f666c5fb
EthanRH11 <157135917+EthanRH11@users.noreply.github.com>
2024-09-03 13:44:48 -0400

    hash map mapped

--- HashMapGenerator.java
+++ HashMapGenerator.java
@@ -1,7 +1,62 @@
 public class HashMapGenerator {
         public static void main(String[] args) {
-//            HashMap<Character> public void alphabetMap = new HashMap<>();
-            //need to figure out how to fill this map based on ASCII values and Vigenere cipher rules!!
+            HashMap<char, int> public void alphabetMap = new HashMap<>();
+
+
+            alphabetMap.put('a', 1);
+            alphabetMap.put('b', 2);
+            alphabetMap.put('c', 3);
+            alphabetMap.put('d', 4);
+            alphabetMap.put('e', 5);
+            alphabetMap.put('f', 6);
+            alphabetMap.put('g', 7);
+            alphabetMap.put('h', 8);
+            alphabetMap.put('i', 9);
+            alphabetMap.put('j', 10);
+            alphabetMap.put('k', 11);
+            alphabetMap.put('l', 12);
+            alphabetMap.put('m', 13);
+            alphabetMap.put('n', 14);
+            alphabetMap.put('o', 15);
+            alphabetMap.put('p', 16);
+            alphabetMap.put('q', 17);
+            alphabetMap.put('r', 18);
+            alphabetMap.put('s', 19);
+            alphabetMap.put('t', 20);
+            alphabetMap.put('u', 21);
+            alphabetMap.put('v', 22);
+            alphabetMap.put('w', 23);          
+            alphabetMap.put('x', 24);
+            alphabetMap.put('y', 25);
+            alphabetMap.put('z', 26);
+            alphabetMap.put('A', 2);
+            alphabetMap.put('B', 4);
+            alphabetMap.put('C', 6);
+            alphabetMap.put('D', 8);
+            alphabetMap.put('E', 10);
+            alphabetMap.put('F', 12);
+            alphabetMap.put('G', 14);
+            alphabetMap.put('H', 16);
+            alphabetMap.put('I', 18);
+            alphabetMap.put('J', 20);
+            alphabetMap.put('K', 22);
+            alphabetMap.put('L', 24);
+            alphabetMap.put('M', 26);
+            alphabetMap.put('N', 28);
+            alphabetMap.put('O', 30);
+            alphabetMap.put('P', 32);
+            alphabetMap.put('Q', 34);
+            alphabetMap.put('R', 36);
+            alphabetMap.put('S', 38);
+            alphabetMap.put('T', 40);
+            alphabetMap.put('U', 42);
+            alphabetMap.put('V', 44);
+            alphabetMap.put('W', 46);
+            alphabetMap.put('X', 48);
+            alphabetMap.put('Y', 50);
+            alphabetMap.put('Z', 52);
+
+
 
 
         }
