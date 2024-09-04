d665403aa1ec56cefc0c03052ca0052fd85ec146
DerrekNguyen <mnguye21@kent.edu>
2024-09-03 22:16:58 -0400

    Added all conversion functions for Blowfish

--- Blowfish.java
+++ Blowfish.java
@@ -4,6 +4,69 @@
 // Public Class BLOWFISH
 public class Blowfish {
 
+    // Derrek ~ CONVERT AN ASCII STANDARD STRING TO A STRING OF HEXADECIMAL NUMBERS
+    static String StrToHex(String input) {
+        
+        StringBuilder result =  new StringBuilder("");
+
+        for (char ch : input.toCharArray()) {
+            // Converts each character of the input string to integer
+            // then convert that integer to hexadecimal and add to "result"
+            result.append(Integer.toHexString((int)ch));
+        }
+
+        return result.toString();
+    }
+
+    // Derrek ~ CONVERT FROM HEXADECIMAL STRING TO BINARY STRING
+    static String HexToBin(String input) {
+
+        StringBuilder result =  new StringBuilder("");
+        StringBuffer tempStr =  new StringBuffer(""); // stores 4 digits of binary at a time
+
+        for (char ch : input.toCharArray()) {
+
+            // Converts each character to a String first
+            // then convert that Hex String to an Decimal integer
+            // then convert that a Binary String
+            // then add that String to "tempStr"
+            tempStr.append(
+                Integer.toBinaryString(
+                    Integer.parseInt(
+                        String.valueOf(ch), 10)));
+            
+
+            // Add leading 0s
+            while (tempStr.length() < 4) {
+                tempStr.insert(0, "0");
+            }
+
+            // Add the 4 binary digits to "result"
+            result.append(tempStr);
+            // and reset "tempStr"
+            tempStr.setLength(0);
+
+        }
+
+        return result.toString();
+    }
+
+    // Derrek ~ CONVERT FROM BINARY STRING TO HEXADECIMAL STRING
+    static String BinToHex(String input) {
+        
+        StringBuilder result =  new StringBuilder("");
+        String tempStr = "";
+
+        // takes 4 digits at a time and make it hexadecimal, adds it to "result"
+        for (int i = 0; i < input.length(); i = i + 4) {
+            tempStr = input.substring(i, i + 4);
+            result.append(Integer.toHexString(Integer.parseInt(tempStr)));
+        }
+
+        return result.toString();
+    }
+
+
     // AC ~ START OF XOR FUNCTION THAT WILL COMPARE TWO STRINGS OF BINARY
     static String[] xOr(String[] lhs, String[] rhs) {
         String[] result = new String[lhs.length];
